8d7f8c6afd3405c6fbc79eaa924960e18c3f0674
Nicole Wu (Gorilla) <nicole.wu@gorilla-technology.com>
2025-06-19 14:53:06 +0800

    Optimize performance

--- main.cpp
+++ main.cpp
@@ -1,61 +1,82 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <typeinfo>
+#include <cstring>
+#include <math.h>
 
 using namespace std;
 
-string addStrings (string line, string sumStr) 
+const int maxcharArrSize = 101;
+const int maxIntArrSize = 12;
+const int digitSize = 9;
+
+void convertChar2Int(char input[], int output[]) 
 {
-    bool isCarry = false;
-    if (line.size() > sumStr.size()) {
-            swap(line, sumStr);
+    int len = strlen(input);
+    int count = maxIntArrSize - 1;
+    int startIndex = len - digitSize - 1;
+
+    for (int i = startIndex; i >= 0; i-=digitSize) {
+        sscanf(input + i, "%9d", &output[count]);
+        count -= 1;
     }
 
-    for (int i = sumStr.size() - 1 ; i >= 0; i--) {
-        char digitA = line[i];
-        char digitB = sumStr[i];
+    int remainder = len % digitSize - 1;
+    if (remainder > 0) {
+        char format[4];
+        sprintf(format, "%%%dd", remainder);
+        sscanf(input, format, &output[count]);
+    }
+}
 
-        // add two characters
-        int sum = (digitA - '0') + (digitB - '0');
-        if (isCarry) {
-            sum += 1;
-        }
-
-        // check if it carries or not
-        if (sum / 10 > 0) {
-            isCarry = true;
+void SumUp(int sum[], int addend[])
+{
+    int carry = 0;
+    int maxResult = pow(10, digitSize);
+    for (int i = maxIntArrSize - 1; i >= 0; i--) {
+        sum[i] += addend[i] + carry;
+        if ((sum[i]) >= maxResult) {
+            sum[i] = sum[i] % maxResult;
+            carry = 1;
         }
         else {
-            isCarry = false;
-        } 
-
-        // update the sum
-        sumStr[i] = sum % 10 + '0';
+            carry = 0;
+        }
     }
+}
 
-    if (isCarry) {
-        sumStr = '1' + sumStr;
+void printResult(int result[])
+{
+    bool isPadding = false;
+    for (int i = 0; i < maxIntArrSize; i++) {
+        if (result[i] == 0 && !isPadding) {
+            continue;
+        }
+        else {
+            isPadding ? printf("%09lld", result[i]) : printf("%d", result[i]);
+            isPadding = true;
+        }
     }
+}
 
-    return sumStr;
+void sumUpBigNumber (char line[], int sum[], int addend[]) {
+    while(fgets(line, maxcharArrSize, stdin)) {
+        if (strcmp(line, "0\0") == 0) {
+            break;
+        }
+
+        memset(addend, 0, sizeof(maxIntArrSize));
+        convertChar2Int(line, addend);
+        SumUp(sum, addend);
+    }
+    printResult(sum);
 }
 
 int main()
 {
-    string line;
-    string sum;
+    char line[maxcharArrSize];
+    int sum[maxIntArrSize] = {0};
+    int addend[maxIntArrSize] = {0};
 
-    getline(cin, sum);
+    sumUpBigNumber(line, sum, addend);
 
-    while (getline(cin, line)) {
-        if (line == "0") {
-            break;
-        }
-
-        sum = addStrings(line, sum);
-    }
-
-    cout << sum <<endl;
     return 0;
 }
